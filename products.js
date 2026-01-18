23c4f4496272fff99d50ec7f89b4b5904b764dcf
daliluktrafel-spec <daliluktrafel@gmail.com>
2026-01-18 16:51:51 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,10 @@
+const products = [
+    {id:1, name:"سكر", price:"5000 ريال", store:"متجر الوفاء", status:"متوفر", image:"https://via.placeholder.com/200x120?text=سكر"},
+    {id:2, name:"شاي", price:"3000 ريال", store:"متجر الوفاء", status:"غير متوفر", image:"https://via.placeholder.com/200x120?text=شاي"},
+    {id:3, name:"زيت", price:"12000 ريال", store:"متجر الخير", status:"متوفر", image:"https://via.placeholder.com/200x120?text=زيت"},
+    {id:4, name:"أرز", price:"8000 ريال", store:"متجر الخير", status:"متوفر", image:"https://via.placeholder.com/200x120?text=أرز"},
+    {id:5, name:"طحين", price:"6000 ريال", store:"متجر الوفاء", status:"متوفر", image:"https://via.placeholder.com/200x120?text=طحين"},
+    {id:6, name:"ملح", price:"1500 ريال", store:"متجر الخير", status:"غير متوفر", image:"https://via.placeholder.com/200x120?text=ملح"},
+    {id:7, name:"حليب", price:"7000 ريال", store:"متجر الوفاء", status:"متوفر", image:"https://via.placeholder.com/200x120?text=حليب"},
+    {id:8, name:"بيض", price:"4000 ريال", store:"متجر الخير", status:"متوفر", image:"https://via.placeholder.com/200x120?text=بيض"}
+];
