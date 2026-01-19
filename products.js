04257b0cbf1a39df164635445a6d8c4298ebf295
daliluktrafel-spec <daliluktrafel@gmail.com>
2026-01-19 22:25:51 +0300

    Update products.js

--- products.js
+++ products.js
@@ -6,5 +6,10 @@ const products = [
     {id:5, name:"طحين", price:"6000 ريال", store:"متجر الوفاء", status:"متوفر", image:"https://via.placeholder.com/200x120?text=طحين"},
     {id:6, name:"ملح", price:"1500 ريال", store:"متجر الخير", status:"غير متوفر", image:"https://via.placeholder.com/200x120?text=ملح"},
     {id:7, name:"حليب", price:"7000 ريال", store:"متجر الوفاء", status:"متوفر", image:"https://via.placeholder.com/200x120?text=حليب"},
-    {id:8, name:"بيض", price:"4000 ريال", store:"متجر الخير", status:"متوفر", image:"https://via.placeholder.com/200x120?text=بيض"}
+    {id:8, name:"بيض", price:"4000 ريال", store:"متجر الخير", status:"متوفر", image:"https://via.placeholder.com/200x120?text=بيض"},
+
+    // منتجات متجر "عالم نور"
+    {id:9, name:"عطر عاشق العود", price:"60 ر.س", store:"عالم نور", status:"متوفر", image:"https://via.placeholder.com/200x120?text=عطر+عاشق+العود"},
+    {id:10, name:"احذية سيليكونية بناتي", price:"4500 ري", store:"عالم نور", status:"متوفر", image:"https://via.placeholder.com/200x120?text=احذية+بناتي"},
+    {id:11, name:"شنط يد نسائية", price:"11500 ر.ي", store:"عالم نور", status:"متوفر", image:"https://via.placeholder.com/200x120?text=شنط+نسائية"}
 ];
