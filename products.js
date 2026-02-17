a4e6cc803cf5bfe15d79eefaf1790997fdc6e5e1
daliluktrafel-spec <daliluktrafel@gmail.com>
2026-02-17 13:57:52 +0300

    Update products.js

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ let products = []; // قائمة المنتجات الفارغة
 // دالة لتحميل المنتجات من Firebase
 function loadProductsFromFirebase() {
     const productsRef = firebase.database().ref('products'); // مجلد المنتجات في Firebase
-    productsRef.on('value', (snapshot) => {
+   productsRef.once('value', (snapshot) => {
         const data = snapshot.val();
         products = data ? Object.values(data) : [];
         // بعد تحميل المنتجات، نعيد عرضها في الصفحة
